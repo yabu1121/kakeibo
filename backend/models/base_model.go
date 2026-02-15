60a29bab967cef335b32c1530a05f9c23e2af0d7
yabu1121 <n.hayato0122@outlook.jp>
2026-02-15 20:28:22 +0900

    handlers http reset

--- backend/models/base_model.go
+++ backend/models/base_model.go
@@ -9,7 +9,7 @@ import (
 
 // BaseModel GORMモデルのベース（JSONタグ付き）
 type BaseModel struct {
-	ID        uuid.UUID      `json:"id" gorm:"primarykey;autoIncrement"`
+	ID        uuid.UUID      `json:"id" gorm:"primaryKey;type:char(36)"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
--- backend/models/report.go
+++ backend/models/report.go
@@ -2,17 +2,17 @@ package models
 
 import (
 	"time"
-
 	"github.com/google/uuid"
+	"gorm.io/datatypes"
 )
 
 type Report struct {
 	BaseModel
-	TargetMonth time.Time `json:"target_month" gorm:"not null;index"`
-	TotalExpense uint64    `json:"total_expense" gorm:"not null"`
-	TotalPublicFee uint64    `json:"total_public_fee" gorm:"not null"`
-	TotalSubscription uint64    `json:"total_subscription" gorm:"not null"`
-	CategoryBreakdown uint64 `json:"category_breakdown"`
+	TargetMonth       time.Time      `json:"target_month" gorm:"not null;index"`
+	TotalExpense      uint64         `json:"total_expense" gorm:"not null"`
+	TotalPublicFee    uint64         `json:"total_public_fee" gorm:"not null"`
+	TotalSubscription uint64         `json:"total_subscription" gorm:"not null"`
+	CategoryBreakdown datatypes.JSON `json:"category_breakdown" gorm:"type:json"`
 
 	UserID uuid.UUID `json:"user_id" gorm:"type:char(36);not null;index"`
 	User   User      `json:"user" gorm:"foreignKey:UserID"`
